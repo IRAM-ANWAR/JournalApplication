0af81a5c2240e731aff15843a5caa72b198f7032
iram0 <iram09999anwar@gmail.com>
2025-02-05 17:28:06 +0530

    Retry strategy

--- src/main/java/com/journal/JournalApplication.java
+++ src/main/java/com/journal/JournalApplication.java
@@ -1,10 +1,16 @@
 package com.journal;// Base Package for component scan
 
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.hc.client5.http.HttpRequestRetryStrategy;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.http.HttpRequest;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.protocol.HttpContext;
+import org.apache.hc.core5.util.TimeValue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -28,8 +34,8 @@ import lombok.extern.slf4j.Slf4j;
 public class JournalApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext aonfigurableApplicationContext = SpringApplication
-		        .run(JournalApplication.class, args);
+		ConfigurableApplicationContext aonfigurableApplicationContext = SpringApplication.run(JournalApplication.class,
+		        args);
 		log.info(List.of(aonfigurableApplicationContext.getEnvironment().getActiveProfiles()).toString());
 
 	}
@@ -50,7 +56,25 @@ public class JournalApplication {
 	@ConditionalOnProperty(value = "custom-http-pooling", havingValue = "true", matchIfMissing = false)
 	RestTemplate restTemplate() {
 		CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(configureConnectionManager())
-		        .build();
+		        .setRetryStrategy(new HttpRequestRetryStrategy() {
+
+			        @Override
+			        public TimeValue getRetryInterval(HttpResponse response, int execCount, HttpContext context) {
+				        return TimeValue.ofMicroseconds(1000);
+			        }
+
+			        @Override
+			        public boolean retryRequest(HttpRequest request, IOException exception, int execCount,
+			                HttpContext context) {
+				        return execCount < 3;
+			        }
+
+			        @Override
+			        public boolean retryRequest(HttpResponse response, int execCount, HttpContext context) {
+				        return execCount < 3;
+			        }
+
+		        }).build();
 		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
 		factory.setConnectTimeout(5000); // 5 seconds
 		factory.setReadTimeout(5000); // 5 seconds
